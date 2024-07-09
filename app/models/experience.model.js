c68117e00800c6058a70778f7a335f5ecb91927e
pranavi-k <ramapranavi.koganti@eagles.oc.edu>
2024-07-09 00:28:18 -0400

    changes in education,experience and resume models

--- app/models/experience.model.js
+++ app/models/experience.model.js
@@ -1,36 +1,48 @@
 module.exports = (sequelize, Sequelize) => {
-    const Experience = sequelize.define("experience", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+  const Experience = sequelize.define("experience", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    employer: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    from_date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    to_date: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    position: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    information: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    }
+  }, {
+    hooks: {
+      beforeCreate: (experience, options) => {
+        if (!experience.to_date) {
+          experience.to_date = null;
+        }
       },
-      employer: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      from_date: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      to_date: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
-      position: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      location: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      information: {
-        type: Sequelize.TEXT,
-        allowNull: true,
+      beforeUpdate: (experience, options) => {
+        if (!experience.to_date) {
+          experience.to_date = null;
+        }
       }
+    }
     });
-  
-    return Experience;
-  };
-  
+
+  return Experience;
+};
