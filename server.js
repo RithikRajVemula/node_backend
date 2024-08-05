fac604a029455bc4a4f7f2d8ec9f8ae0557ebb3b
RithikRajVemula <150193329+RithikRajVemula@users.noreply.github.com>
2024-08-05 19:29:56 -0400

    Add resume comments routes and controllers

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ require("./app/routes/user.routes.js")(app);
 require("./app/routes/resume.routes.js")(app);
 require("./app/routes/skill.routes.js")(app);
 require("./app/routes/pdf.routes.js")(app);
+require("./app/routes/resumeComment.routes.js")(app);
 
 const PORT = process.env.PORT || 3200;
 if (process.env.NODE_ENV !== "test") {
